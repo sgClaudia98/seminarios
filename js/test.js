626bc2afdee7a258f427542ad329f1461942efd9
Claudia SG <63687204+sgClaudia98@users.noreply.github.com>
2023-02-28 17:40:51 +0100

    asd

--- js/test.js
+++ js/test.js
@@ -1,9 +1,9 @@
 /**
   test.js
-  Ejemplo Three.js_r140: Cubo RGB con iluminacion y textura
+  Ejemplo Three.js_r140: Rectangle RGB con iluminacion y textura
 
-  Cubo con color por vertice y mapa de uvs usando la clase BufferGeometry.
-  La textura es una unica imagen en forma de cubo desplegado en cruz horizontal.
+  Rectangle con color por vertice y mapa de uvs usando la clase BufferGeometry.
+  La textura es una unica imagen en forma de rectangle desplegado en cruz horizontal.
   Cada cara se textura segun mapa uv en la textura.
   En sentido antihorario las caras son:
     Delante:   7,0,3,4
@@ -12,19 +12,21 @@
     Izquierda: 6,7,4,5
     Arriba:    3,2,5,4
     Abajo:     0,7,6,1
-  Donde se han numerado de 0..7 los vertices del cubo.
+  Donde se han numerado de 0..7 los vertices del rectangle.
   Los atributos deben darse por vertice asi que necesitamos 8x3=24 vertices pues
   cada vertice tiene 3 atributos de normal, color y uv al ser compartido por 3 caras. 
 
   @author rvivo@upv.es (c) Libre para fines docentes
 */
 
-var renderer, scene, camera, cubo;
+var renderer, scene, camera, rectangle;
 var cameraControls;
 var angulo = -0.01;
 
 init();
-loadSquare(1.0);
+loadBackground();
+loadRectangle(1.0, 1.5, 1);
+loadRectangle(1.0, 1.5, -0.5);
 render();
 
 function init() {
@@ -37,7 +39,7 @@ function init() {
 
   var aspectRatio = window.innerWidth / window.innerHeight;
   camera = new THREE.PerspectiveCamera(50, aspectRatio, 0.1, 100);
-  camera.position.set(1, 1.5, 2);
+  camera.position.set(0, 0, 3);
   camera.lookAt(0, 0, 0);
 
   cameraControls = new THREE.OrbitControls(camera, renderer.domElement);
@@ -46,98 +48,44 @@ function init() {
   window.addEventListener('resize', updateAspectRatio);
 }
 
-function loadCubo(lado) {
+function loadBackground() {
+  var VBack = [
+    -24.0, 0.0, 0.0,
+    24.0, 0.0, 0.0,
+    -24.0, 48.0, 0.0,
+    24.0, 48.0, 0.0,
+  ];
+  scene.add(rectangle);
+}
+
+function loadRectangle(ladoA, ladoB, offsetA) {
   // Instancia el objeto BufferGeometry
   var malla = new THREE.BufferGeometry();
   // Construye la lista de coordenadas y colores por vertice
-  var semilado = lado / 2.0;
+  var semiladoA = ladoA / 2.0;
+  var semiladoB = ladoB / 2.0;
   var coordenadas = [ // 6caras x 4vert x3coor = 72float
     // Front 
-    -semilado, -semilado, semilado, // 7 -> 0
-    semilado, -semilado, semilado,  // 0 -> 1
-    semilado, semilado, semilado,  // 3 -> 2
-    -semilado, semilado, semilado, // 4 -> 3
-    // Right
-    semilado, -semilado, semilado,  // 0 -> 4
-    semilado, -semilado, -semilado,  // 1 -> 5
-    semilado, semilado, -semilado,  // 2 -> 6
-    semilado, semilado, semilado,  // 3 -> 7
-    // Back
-    semilado, -semilado, -semilado,  // 1 -> 8
-    -semilado, -semilado, -semilado, // 6 -> 9
-    -semilado, semilado, -semilado, // 5 ->10
-    semilado, semilado, -semilado,  // 2 ->11
-    // Left
-    -semilado, -semilado, -semilado, // 6 ->12
-    -semilado, -semilado, semilado, // 7 ->13
-    -semilado, semilado, semilado, // 4 ->14
-    -semilado, semilado, -semilado, // 5 ->15
-    // Top
-    semilado, semilado, semilado,  // 3 ->16
-    semilado, semilado, -semilado,  // 2 ->17
-    -semilado, semilado, -semilado, // 5 ->18
-    -semilado, semilado, semilado, // 4 ->19
-    // Bottom
-    semilado, -semilado, semilado,  // 0 ->20
-    -semilado, -semilado, semilado, // 7 ->21 
-    -semilado, -semilado, -semilado, // 6 ->22
-    semilado, -semilado, -semilado   // 1 ->23
+    -semiladoA + offsetA, -semiladoB, 0.01, // 7 -> 0
+    semiladoA + offsetA, -semiladoB, 0.01,  // 0 -> 1
+    semiladoA + offsetA, semiladoB, 0.01,  // 3 -> 2
+    -semiladoA + offsetA, semiladoB, 0.01, // 4 -> 3
   ]
   var colores = [ // 24 x3
     0, 0, 0,   // 7
     1, 0, 0,   // 0
     1, 1, 0,   // 3
     0, 1, 0,   // 4
-
-    1, 0, 0,   // 0
-    1, 0, 1,   // 1
-    1, 1, 1,   // 2
-    1, 1, 0,   // 3
-
-    1, 0, 1,   // 1
-    0, 0, 1,   // 6
-    0, 1, 1,   // 5
-    1, 1, 1,   // 2
-
-    0, 0, 1,   // 6
-    0, 0, 0,   // 7
-    0, 1, 0,   // 4
-    0, 1, 1,   // 5
-
-    1, 1, 0,   // 3
-    1, 1, 1,   // 2
-    0, 1, 1,   // 5
-    0, 1, 0,   // 4
-
-    1, 0, 0,   // 0
-    0, 0, 0,   // 7
-    0, 0, 1,   // 6
-    1, 0, 1    // 1
   ]
   var normales = [ // 24 x3
     0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1,      // Front
-    1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0,      // Right
-    0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1,  // Back 
-    -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0,  // Left
-    0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0,      // Top 
-    0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0   // Bottom
   ];
   var uvs = [  // 24 x2
     // Front
     0 / 4, 1 / 3, 1 / 4, 1 / 3, 1 / 4, 2 / 3, 0 / 4, 2 / 3, // 7,0,3,4
-    1 / 4, 1 / 3, 2 / 4, 1 / 3, 2 / 4, 2 / 3, 1 / 4, 2 / 3, // 0,1,2,3
-    2 / 4, 1 / 3, 3 / 4, 1 / 3, 3 / 4, 2 / 3, 2 / 4, 2 / 3, // 1,6,5,2
-    3 / 4, 1 / 3, 4 / 4, 1 / 3, 4 / 4, 2 / 3, 3 / 4, 2 / 3, // 6,7,4,5
-    1 / 4, 2 / 3, 2 / 4, 2 / 3, 2 / 4, 3 / 3, 1 / 4, 3 / 3, // 3,2,5,4
-    1 / 4, 1 / 3, 1 / 4, 0 / 3, 2 / 4, 0 / 3, 2 / 4, 1 / 3   // 0,7,6,1
   ];
   var indices = [ // 6caras x 2triangulos x3vertices = 36
     0, 1, 2, 2, 3, 0,    // Front
-    4, 5, 6, 6, 7, 4,    // Right 
-    8, 9, 10, 10, 11, 8,  // Back
-    12, 13, 14, 14, 15, 12, // Left
-    16, 17, 18, 18, 19, 16, // Top
-    20, 21, 22, 22, 23, 20  // Bottom
   ];
 
   scene.add(new THREE.DirectionalLight());
@@ -155,61 +103,10 @@ function loadCubo(lado) {
 
   // Construye el objeto grafico 
   console.log(malla);   //-> Puedes consultar la estructura del objeto
-  cubo = new THREE.Mesh(malla, material);
+  rectangle = new THREE.Mesh(malla, material);
 
   // Añade el objeto grafico a la escena
-  scene.add(cubo);
-}
-
-
-function loadSquare(lado) {
-  // Instancia el objeto BufferGeometry
-  var malla = new THREE.BufferGeometry();
-  // Construye la lista de coordenadas y colores por vertice
-  var semilado = lado / 2.0;
-  var coordenadas = [ // 6caras x 4vert x3coor = 72float
-    // Right
-    semilado, -semilado, semilado,  // 0 -> 4
-    semilado, -semilado, -semilado,  // 1 -> 5
-    semilado, semilado, -semilado,  // 2 -> 6
-    semilado, semilado, semilado,  // 3 -> 7
-  ]
-  var colores = [ // 24 x3
-
-    1, 0, 0,   // 0
-    1, 0, 1,   // 1
-    1, 1, 1,   // 2
-    1, 1, 0,   // 3
-  ]
-  var normales = [ // 24 x3
-    1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0,      // Right
-  ];
-  var uvs = [  // 24 x2
-    1 / 4, 1 / 3, 2 / 4, 1 / 3, 2 / 4, 2 / 3, 1 / 4, 2 / 3, // 0,1,2,3
-  ];
-  var indices = [ // 6caras x 2triangulos x3vertices = 36
-    4, 5, 6, 6, 7, 4,    // Right 
-  ];
-
-  scene.add(new THREE.DirectionalLight());
-
-  // Geometria por att arrays en r140
-  malla.setIndex(indices);
-  malla.setAttribute('position', new THREE.Float32BufferAttribute(coordenadas, 3));
-  malla.setAttribute('normal', new THREE.Float32BufferAttribute(normales, 3));
-  malla.setAttribute('color', new THREE.Float32BufferAttribute(colores, 3));
-  malla.setAttribute('uv', new THREE.Float32BufferAttribute(uvs, 2));
-
-  // Configura un material
-  var textura = new THREE.TextureLoader().load('images/ilovecg.png');
-  var material = new THREE.MeshLambertMaterial({ vertexColors: true, map: textura, side: THREE.DoubleSide });
-
-  // Construye el objeto grafico 
-  console.log(malla);   //-> Puedes consultar la estructura del objeto
-  square = new THREE.Mesh(malla, material);
-
-  // Añade el objeto grafico a la escena
-  scene.add(square);
+  scene.add(rectangle);
 }
 
 function updateAspectRatio() {
@@ -222,13 +119,13 @@ function update() {
   // Cambios para actualizar la camara segun mvto del raton
   cameraControls.update();
 
-  // Movimiento propio del cubo
-  cubo.rotation.y += angulo;
-  cubo.rotation.x += angulo / 2;
+  // Movimiento propio del rectangle
+  rectangle.rotation.y += angulo;
+  rectangle.rotation.x += angulo / 2;
 }
 
 function render() {
-  requestAnimationFrame(render);
-  update();
+  //requestAnimationFrame(render);
+  //update();
   renderer.render(scene, camera);
 }
